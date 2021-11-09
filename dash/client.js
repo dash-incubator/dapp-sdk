9b2cfe9407f7b47d626f7362a68f76b03ac4815a
Israel Casillas Jr <ICJR@users.noreply.github.com>
2021-11-09 14:46:05 -0800

    chore: comment dcg nodes in favor for global testnet

--- dash/client.js
+++ dash/client.js
@@ -9,15 +9,15 @@ const connect = (options = {}) => {
     options.wallet = options.wallet || {};
 
     if (testnet) {
-        options.dapiAddresses = options.dapiAddresses || [
-            '34.217.23.70:3000',
-            '34.222.102.137:3000',
-            '34.209.166.42:3000',
-            '18.236.128.49:3000',
-            '35.163.99.20:3000',
-            '34.215.67.224:3000',
-            '34.211.244.117:3000'
-        ];
+        // options.dapiAddresses = options.dapiAddresses || [
+        //     '34.217.23.70:3000',
+        //     '34.222.102.137:3000',
+        //     '34.209.166.42:3000',
+        //     '18.236.128.49:3000',
+        //     '35.163.99.20:3000',
+        //     '34.215.67.224:3000',
+        //     '34.211.244.117:3000'
+        // ];
         options.network = 'testnet';
         options.wallet.unsafeOptions = {
             skipSynchronizationBeforeHeight: 506776

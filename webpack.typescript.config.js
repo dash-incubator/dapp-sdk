8bc65061e30d6a6a1ae10b60856a7ddb0ecb4839
Israel Casillas Jr <ICJR@users.noreply.github.com>
2022-01-26 08:54:44 -0800

    chore: cleanup

--- webpack.typescript.config.js
+++ webpack.typescript.config.js
@@ -1,4 +1,4 @@
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const glob = require('glob');
 const webpack = require('webpack');
